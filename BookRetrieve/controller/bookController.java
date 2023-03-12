e061bbf62382b113c634a03ec9358bf80c5eade8
AustinH432 <124899792+AustinH432@users.noreply.github.com>
2023-03-12 11:02:25 -0400

    Add files via upload
    Functional filtering for genre, top 10 sellers, of a rating, and discounting.

---
+++ BookRetrieve/controller/bookController.java
@@ -0,0 +1,53 @@
+package BookRetrieve.controller;
+
+import BookRetrieve.model.Book;
+import BookRetrieve.repository.repo;
+import BookRetrieve.service.bookService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+import java.util.List;
+
+@RestController
+public class bookController {
+    @Autowired
+    repo repoTest;
+
+    @Autowired
+    bookService bookServ = new bookService();
+    @PostMapping("/bookss")
+    public Book save(@RequestBody Book book) {
+        return repoTest.save(book);
+    }
+
+    @GetMapping("/books-list")
+    public List<Book> getAllBooks() {
+       return repoTest.findAll();
+    }
+
+    @GetMapping("/books-list-genre/{genre}")
+    public List<Book> findByGenreContaining(@PathVariable("genre") String genre) {
+        return repoTest.findByGenre(genre);
+    }
+
+
+   @GetMapping("/books-list-rating/{minimumrating}")
+    public List<Book> findbyrating(@PathVariable("minimumrating") double minimumrating) {
+       return repoTest.findByratingGreaterThanEqual(minimumrating);
+    }
+
+    @GetMapping("/books-list-topsellers")
+    public List<Book> topSellers() {
+       return repoTest.findTop10BySalesGreaterThanOrderBySalesDesc(0);
+    }
+
+    @GetMapping("/books-list-publisher/{publisher}")
+    public List<Book> findByPublisher(@PathVariable("publisher") String publisher) {
+        return repoTest.findBypublisher(publisher);
+    }
+
+    @PatchMapping("/books-update/{publisher}/{percent}")
+    public List<Book> updatePublisher(@PathVariable("publisher") String publisher, @PathVariable("percent") double discountPercent) {
+        return bookServ.updatePublisherDiscount(publisher, discountPercent);
+    }
+
+}
---
+++ BookRetrieve/mainApplication.java
@@ -0,0 +1,16 @@
+package BookRetrieve;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class mainApplication {
+
+    public static void main(String[] args) {
+
+        SpringApplication.run(mainApplication.class, args);
+
+
+    }
+}
+
